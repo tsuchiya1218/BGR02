a6e5cc812fd188a4481ad2a90a18b2d0fa1e7dde
20jy0224 <20jy0224@jec.ac.jp>
2022-01-14 11:01:52 +0900

    index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="jp">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/common.css" rel="stylesheet" type ="text/css">
+        <title>とっぷぺぇじ</title>
+    </head>
+    <body>
+        <?php require_once("header.php"); ?>
+        <main>
+            <h2>トップページ</h2>
+            <p>ここはトップページです</p>
+        </main>
+        <?php require_once("footer.php"); ?>
+    </body>
+</html>
